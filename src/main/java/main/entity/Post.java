d0cc8b4d6f14c47a29581acb3a8e1ee6618b8f07
tsigereda-hagos <150645322+tsigereda-hagos@users.noreply.github.com>
2024-05-29 01:36:25 -0500

    lab1

--- src/main/java/main/entity/Post.java
+++ src/main/java/main/entity/Post.java
@@ -1,5 +1,12 @@
 package main.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
     long id;
     String title;
